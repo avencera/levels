0ef44c7f185e2d08dea6ab91adec1d6e67269574
Praveen Perera <praveen@avencera.com>
2021-12-14 23:10:30 -0500

    SAVEPOINT

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,64 @@
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use ringbuf::RingBuffer;
+use cpal::{
+    traits::{DeviceTrait, HostTrait, StreamTrait},
+    SampleFormat,
+};
+use ringbuf::{Consumer, Producer, RingBuffer};
 
-const SAMPLE: i32 = 48;
-const MS: i32 = 100;
-const CHANNELS: i32 = 2;
+const INTERVAL: u16 = 100;
+const LATENCY: u16 = INTERVAL / 2;
 
-const READ_BUFFER: usize = (((SAMPLE + 1) * CHANNELS) * MS) as usize;
-const WRITE_BUFFER: usize = ((SAMPLE + 1) * MS * CHANNELS) as usize;
+struct App {
+    device: Device,
+    channels: u16,
+    sample_rate: u32,
+    read_at_a_time: usize,
+    producer: Producer<f32>,
+    consumer: Consumer<f32>,
+    sample_format: SampleFormat,
+}
+
+impl App {
+    fn new() -> Self {
+        let host = cpal::default_host();
+        let device = host
+            .default_input_device()
+            .expect("no input device available");
+
+        let config = device
+            .default_input_config()
+            .expect("Failed to get default input config");
+
+        let sample_rate = config.sample_rate().0;
+        let channels = config.channels();
+
+        let read_at_a_time =
+            ((sample_rate as f32 / 1000.0) * (channels * INTERVAL) as f32).ceil() as usize;
+
+        let write_ahead =
+            ((sample_rate as f32 / 1000.0) * (channels * LATENCY) as f32).ceil() as usize;
+
+        let ring = RingBuffer::new(read_at_a_time + write_ahead);
+        let (producer, consumer) = ring.split();
+
+        Self {
+            channels,
+            sample_rate,
+            read_at_a_time,
+            producer,
+            consumer,
+            sample_format: config.sample_format(),
+        }
+    }
+
+    fn handle_data(&mut self, data: &[f32]) {
+        if self.producer.push_slice(data) < data.len() {
+            eprintln!("buffer too slow")
+        }
+    }
+}
 
 fn main() -> eyre::Result<()> {
     color_eyre::install()?;
-
-    let host = cpal::default_host();
-    let device = host
-        .default_input_device()
-        .expect("no input device available");
-
-    // let mut supported_configs_range = device
-    //     .supported_input_configs()
-    //     .expect("error while querying configs");
-
-    let config = device
-        .default_input_config()
-        .expect("Failed to get default input config");
-
-    println!("Default input config: {:?}", config);
-
-    println!("CHANNELS: {}", config.channels());
-    println!("SAMPLE RATE: {}", config.sample_rate().0);
-    println!("SAMPLE FORMAT: {:#?}", config.sample_format());
-
-    let err_fn = move |err| {
-        eprintln!("an error occurred on stream: {}", err);
-    };
-
-    let ring = RingBuffer::new(WRITE_BUFFER);
-    let (mut producer, mut consumer) = ring.split();
-
-    let input_data_fn = move |data: &[f32], _: &cpal::InputCallbackInfo| {
-        if producer.push_slice(data) < data.len() {
-            eprintln!("buffer too slow")
-        }
-    };
 
     let stream = match config.sample_format() {
         cpal::SampleFormat::F32 => {
@@ -61,14 +78,11 @@ fn main() -> eyre::Result<()> {
 
     stream.play()?;
 
+    let mut input = [0.0_f32; READ_BUFFER];
     let thread = std::thread::spawn(move || loop {
         use std::cmp::Ordering::Equal;
 
-        let mut input = [0.0_f32; READ_BUFFER];
-
-        consumer.pop_slice(input.as_mut_slice());
-
-        println!("BUFFER LEFT: {}", consumer.remaining());
+        consumer.pop_slice(&mut input);
 
         let avg: f32 = input.iter().sum::<f32>() / input.len() as f32;
 
