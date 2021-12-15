840ae4dfd79958cd10e76b64385ae707b34ed6fa
Praveen Perera <praveen@avencera.com>
2021-12-14 22:36:26 -0500

    Get the amplitude of input continuously

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use ringbuf::RingBuffer;
 
-const SAMPLE: i32 = 44;
-const MS: i32 = 50;
+const SAMPLE: i32 = 48;
+const MS: i32 = 100;
+const CHANNELS: i32 = 2;
 
-const SIZE: usize = ((SAMPLE + 6) * MS) as usize;
-const BUFFER: usize = SIZE * 2 as usize;
+const READ_BUFFER: usize = (((SAMPLE + 1) * CHANNELS) * MS) as usize;
+const WRITE_BUFFER: usize = ((SAMPLE + 1) * MS * CHANNELS) as usize;
 
 fn main() -> eyre::Result<()> {
     color_eyre::install()?;
@@ -22,18 +23,23 @@ fn main() -> eyre::Result<()> {
     let config = device
         .default_input_config()
         .expect("Failed to get default input config");
+
     println!("Default input config: {:?}", config);
+
+    println!("CHANNELS: {}", config.channels());
+    println!("SAMPLE RATE: {}", config.sample_rate().0);
+    println!("SAMPLE FORMAT: {:#?}", config.sample_format());
 
     let err_fn = move |err| {
         eprintln!("an error occurred on stream: {}", err);
     };
 
-    let ring = RingBuffer::new(BUFFER + 1024);
+    let ring = RingBuffer::new(WRITE_BUFFER);
     let (mut producer, mut consumer) = ring.split();
 
     let input_data_fn = move |data: &[f32], _: &cpal::InputCallbackInfo| {
-        if producer.push_slice(data) < 512 {
-            eprintln!("output stream fell behind: try increasing latency");
+        if producer.push_slice(data) < data.len() {
+            eprintln!("buffer too slow")
         }
     };
 
@@ -58,9 +64,13 @@ fn main() -> eyre::Result<()> {
     let thread = std::thread::spawn(move || loop {
         use std::cmp::Ordering::Equal;
 
-        let mut input = [0.0_f32; SIZE];
+        let mut input = [0.0_f32; READ_BUFFER];
 
         consumer.pop_slice(input.as_mut_slice());
+
+        println!("BUFFER LEFT: {}", consumer.remaining());
+
+        let avg: f32 = input.iter().sum::<f32>() / input.len() as f32;
 
         let max = input
             .iter()
@@ -72,7 +82,7 @@ fn main() -> eyre::Result<()> {
             .min_by(|a, b| a.partial_cmp(b).unwrap_or(Equal))
             .unwrap();
 
-        println!("MIN: {}, MAX: {}", min, max);
+        println!("AMP MIN: {}, AMP MAX: {}", avg - min, max - avg);
 
         std::thread::sleep(std::time::Duration::from_millis(MS as u64));
     });
@@ -90,23 +100,4 @@ where
     let min = input.iter().min().unwrap();
 
     println!("MIN: {}, MAX: {}", min, max)
-}
-
-fn get_max(mut producer: ringbuf::Producer<f32>, input: &[f32]) {
-    for &sample in input {
-        producer.push(sample).unwrap()
-    }
-
-    // use std::cmp::Ordering::Equal;
-    // let max = input
-    //     .iter()
-    //     .max_by(|a, b| a.partial_cmp(b).unwrap_or(Equal))
-    //     .unwrap();
-
-    // let min = input
-    //     .iter()
-    //     .min_by(|a, b| a.partial_cmp(b).unwrap_or(Equal))
-    //     .unwrap();
-
-    // println!("MIN: {}, MAX: {}", min, max)
 }
